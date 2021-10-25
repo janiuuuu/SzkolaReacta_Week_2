a15b3942be31e64c4aed09a3731e8f97b755fe83
janiuuuu <jan.borwin@gmail.com>
2021-10-25 14:41:36 +0200

    Zmiany do zadania pierwszego, tydzien drugi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import react from 'react';
+
+const posts = [
+  { id: 1, title: 'Pilne: Co to był za dzień!', intro: 'Tego świat jeszcze nie widział'},
+  { id: 2, title: 'Wszyscy zazdroszą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie wymyślił'},
+  { id: 3, title: 'Adam Małysz Zgolił wąs',
+    intro: 'Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy'
+  }
+]
+
+const styleComponents = {
+  backgroundColor:'#b2bec3',
+  border:'#080a0a 1px solid',
+  padding:10,
+  fontSize:15,
+  textAlign:'left',
+  marginTop: '0.67em',
+  marginBottom: '0.67em',
+  marginLeft: 0,
+  marginRight: 0,
+  fontWeight: 'bold',
+  paragraf:{
+    backgroundColor:'#dfe6e9',
+    border:'#080a0a 1px solid',
+    padding:10,
+    fontSize:10,
+    textAlign:'justify',
+    color: 'red',
+    marginTop: 2,
+    marginBottom: 3,
+    marginLeft: 0,
+    marginRight: 0,
+    fontWeight: 'bold',
+
+  }
+};
+
+class BlogTile extends react.Component{
+  render(){
+    const {title, intro} = this.props;
+    return(
+      <div style={styleComponents}>
+        <h2>{title}</h2>
+        <p style={styleComponents.paragraf}>{intro}</p>
+      </div>
+    );
+  }
+
+}
 
 function App() {
+  const checkIntro = (cos) =>{
+    return(
+      <>{cos.substring(0,25) + `...`}</>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {posts.map((elem, index) => (
+         //<div><h2>{elem.title}</h2><p>{checkIntro(elem.intro)}</p></div>
+         <BlogTile key={elem.id} title={elem.title} intro={checkIntro(elem.intro)}/>
+      ))}
     </div>
   );
 }
