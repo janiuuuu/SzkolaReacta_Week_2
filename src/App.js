57b0b5baa527a9e30a3a4633a7105f5b455abf14
janiuuuu <jan.borwin@gmail.com>
2021-10-25 19:10:35 +0200

    Zmiany dotyczace drugiego zadanie z tygodnia drugiego

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import './App.css';
 import react from 'react';
-
-const posts = [
-  { id: 1, title: 'Pilne: Co to był za dzień!', intro: 'Tego świat jeszcze nie widział'},
-  { id: 2, title: 'Wszyscy zazdroszą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie wymyślił'},
-  { id: 3, title: 'Adam Małysz Zgolił wąs',
-    intro: 'Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy'
-  }
-]
+import { v4 as uuidv4 } from 'uuid';
 
 const styleComponents = {
   backgroundColor:'#b2bec3',
@@ -19,30 +12,15 @@ const styleComponents = {
   marginBottom: '0.67em',
   marginLeft: 0,
   marginRight: 0,
-  fontWeight: 'bold',
-  paragraf:{
-    backgroundColor:'#dfe6e9',
-    border:'#080a0a 1px solid',
-    padding:10,
-    fontSize:10,
-    textAlign:'justify',
-    color: 'red',
-    marginTop: 2,
-    marginBottom: 3,
-    marginLeft: 0,
-    marginRight: 0,
-    fontWeight: 'bold',
-
-  }
+  fontWeight: 'bold'
 };
 
-class BlogTile extends react.Component{
+class GeneratorUUID extends react.Component{
   render(){
-    const {title, intro} = this.props;
+    const {uuid} = this.props;
     return(
-      <div style={styleComponents}>
-        <h2>{title}</h2>
-        <p style={styleComponents.paragraf}>{intro}</p>
+      <div style = { styleComponents}>
+        <p>{uuid}</p>
       </div>
     );
   }
@@ -50,17 +28,9 @@ class BlogTile extends react.Component{
 }
 
 function App() {
-  const checkIntro = (cos) =>{
-    return(
-      <>{cos.substring(0,25) + `...`}</>
-    );
-  }
   return (
     <div>
-      {posts.map((elem, index) => (
-         //<div><h2>{elem.title}</h2><p>{checkIntro(elem.intro)}</p></div>
-         <BlogTile key={elem.id} title={elem.title} intro={checkIntro(elem.intro)}/>
-      ))}
+         <GeneratorUUID uuid = {uuidv4()}/>
     </div>
   );
 }
